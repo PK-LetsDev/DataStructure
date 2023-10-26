8e6c2ac0c8faf9287057c6dc1a942396795463aa
Prodev <prodsong12@gmail.com>
2023-10-26 11:49:59 +0700

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -17,3 +17,16 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+s = Stack()  # declare constructor using class stack
+print(s.isEmpty())  # true
+s.push(4)  # [4]
+s.push('dog')  # [4,'dog']
+print(s.peek())  # 'dog'
+s.push(True)  # [4,'dog',True]
+print(s.size())  # [3]
+print(s.isEmpty())  # false
+s.push(8.4)  # [4,'dog',True,8.4]
+print(s.pop())  # [4,'dog',True] = print 8.4 then delete
+print(s.pop())  # [4,'dog'] = print True then delete
+print(s.size())  # [2]
